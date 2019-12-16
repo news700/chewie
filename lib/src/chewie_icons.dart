9af4619e53c2566ebf19c80e19bd742693310494
eddie <news700@gmail.com>
2019-12-16 16:22:16 +0900

    Change Icons

--- lib/src/chewie_icons.dart
+++ lib/src/chewie_icons.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ChewieIcons {
-  static const IconData iconPause = const _IconData(0xe900);
-  static const IconData iconPlay = const _IconData(0xe901);
+  static const IconData iconPause = const _IconData(0xe901);
+  static const IconData iconPlay = const _IconData(0xe902);
   static const IconData volumeOn = const _IconData(0xe903);
-  static const IconData volumeOff = const _IconData(0xe902);
+  static const IconData volumeOff = const _IconData(0xe900);
 }
 
 class _IconData extends IconData {
